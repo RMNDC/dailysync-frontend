5af8fded4e772b4b919151f57eb5b4d987d424e2
Raven <dagohoymarkraven@gmail.com>
2026-04-24 13:46:44 +0800

    Add services import for autofill

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:html' as html;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
